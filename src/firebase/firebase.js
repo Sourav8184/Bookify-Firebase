5bb90072294e863c0360b15570a4b08e1cee775d
Sourav <ksourav662@gmail.com>
2024-06-08 18:47:18 +0530

    Create .env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCssYIb46RmX_YSYmLLUgyCu4LzbjQ_aKM",
-  authDomain: "bookify-8974a.firebaseapp.com",
-  projectId: "bookify-8974a",
-  storageBucket: "bookify-8974a.appspot.com",
-  messagingSenderId: "87240034640",
-  appId: "1:87240034640:web:bb8466ebb345d9d6514455",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
